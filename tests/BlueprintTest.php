2bc9103fbbb975143854b74536b8a564ecc5e3ec
Dannie Hansen <danniehansenweb@gmail.com>
2020-05-11 22:11:27 +0200

    Added empty test

---
+++ tests/BlueprintTest.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace Blueprinting\Tests;
+
+use Orchestra\Testbench\TestCase;
+
+class BlueprintTest extends TestCase
+{
+    /**
+     * Assert blueprint attributes
+     *
+     * @return void
+     */
+    public function testAssert(): void
+    {
+        $this->assertTrue(true);
+    }
+}
